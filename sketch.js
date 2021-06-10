6fc3a2e0915556a8710b9520aaad29e07cc0882f
Pushkarneravetla <80430879+Pushkarneravetla@users.noreply.github.com>
2021-06-10 20:40:54 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,34 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var eng, world;
+var ground,ball;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(400,400);
+  
+  eng = Engine.create();
+  world = eng.world;
+
+  var pushkar = 
+  {
+  isStatic: true
+  }
+  ground = Bodies.rectangle(200,390,400,20, pushkar);
+  World.add(world,ground);
+
+  ball = Bodies.circle(200, 100, 25);
+  World.add(world, ball);
 }
 
 function draw() {
-  background(255,255,255);  
+  background(255,255,255); 
+  Engine.update(eng);
+  rectMode(CENTER)
+  rect(ground.position.x, ground.position.y, 400, 50);
+  ellipse(ball.position.x, ball.position.y, 25, 25);
+
+  
   drawSprites();
 }
